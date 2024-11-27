8cb92e10874ed9bd54d89f2848f98308242527bd
Marco Paganini <paganini@paganini.net>
2024-11-26 18:22:03 -0800

    Add checking of trailing empty lines/missing newlines.
    - New option: check_empty_line_at_eof will flag files ending in
      empty lines.
    - New option: check_missing_newline_at_eof will flag files ending
      in lines without a newline character at the end.
    - New option: ignore_regex will completely ignore paths matching
      a regular expression. Useful when using "check_all_files".

--- README.md
+++ README.md
@@ -2,23 +2,42 @@
 
 ## Name
 
-check-trailing-spaces - Checks for trailing spaces in text files.
+check-trailing-spaces - Checks for trailing spaces in text files and other conditions.
 
 ## Description
 
 This Github action checks the files being submitted and flags any files
-that contains lines with trailing spaces.
+that contains one of the following conditions:
+
+* Trailing spaces at end of line.
+* Files ending with blank lines.
+* Files ending with a line that does not terminate in a new line character.
 
 ## Arguments
 
 *  `check_all_files`: Set to 1 to check _all_ files in the Github directory,
-   not only the modified files.
+   not only the modified files in the current PR.
+
+*  `check_empty_line_at_eof`: Set to 1 (default) to check for empty lines
+   at the end of the file.
+
+*  `check_missing_newline_at_eof`: Set to 1 (default) to check for files having
+   a last line that does not end in a newline character (common with misconfigured
+   VSCode configurations).
+
+*  `ignore_regex`: A regular expression matching full paths to ignore. Paths
+   usually start with "./", unless `source_directory` is also specified.
+   Example: `"\./content.*"` will ignore all files under the "./content"
+   directory. For further information, look at the man page for the "find"
+   command, option "-regex". Please note that files in the "./.git" directory
+   will be automatically ignored.
 
 * `source_directory`: When `check_all_files` is set, use this directory as
-   the root of the tree to be checked, instead of the Git repository root.
+  the root of the tree to be checked, instead of the Git repository root.
+  This action will only check text files.
 
 * `pr_number`: The number of the PR to check. There's usually no need to
-   change this. The default is the current PR number.
+  change this. The default is the current PR number.
 
 ## Example
 
@@ -37,6 +56,7 @@ jobs:
 
     - name: Check lines in files for trailing whitespaces.
       uses: marcopaganini/check-trailing-spaces@v1
+
     - with: # Most people won't need this.
         check_all_files: 1
         source_directory: /ext
@@ -45,4 +65,3 @@ jobs:
 ## Author
 
 Marco Paganini (https://github.com/marcopaganini)<br>
-Jul/2021
--- entrypoint.sh
+++ entrypoint.sh
@@ -16,47 +16,113 @@ trap "rm -f ${tmpfile}" 0
 # Read and process INPUT_* and other global variables.
 readonly source_dir="${INPUT_SOURCE_DIRECTORY:-.}"
 readonly pr_number="${INPUT_PR_NUMBER:-}"
+readonly check_empty_line_at_eof="${INPUT_CHECK_EMPTY_LINE_AT_EOF:-1}"
+readonly check_missing_newline_at_eof="${INPUT_CHECK_MISSING_NEWLINE_AT_EOF:-1}"
+readonly ignore_regex="${INPUT_IGNORE_REGEX:-}"
 readonly github_repository=${GITHUB_REPOSITORY:-}
 check_all_files="${INPUT_CHECK_ALL_FILES:-0}"
 
-echo "Check for trailing spaces inside text files"
-echo "==========================================="
+function is_text_file() {
+  [[ -s "${1}" ]] && file --mime "${1}" | grep -q " text/"
+}
 
-# If pr_number or github_repository are empty, check all files.
-if [[ -z "${pr_number}" ]] || [[ -z "${github_repository}" ]]; then
-  echo -e "Note: INPUT_PR_NUMBER or GITHUB_REPOSITORY not set. Checking all files.\n"
-  check_all_files=1
-fi
+function main() {
+  local ret=0
 
-# Note: source_dir is ignored unless check_all_files is set.
-if (( check_all_files )); then
-  find "${source_dir}" -name .git -type d -prune -o -type f -print | sort >"${CHANGED_FILES}"
-else
-  # Retrieve list of changes files.
-  URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${pr_number}/files"
-  curl -s -X GET -G "${URL}" | jq -r '.[] | .filename' > "${CHANGED_FILES}"
-fi
+  echo "Check for trailing spaces inside text files"
+  echo "==========================================="
 
-while read -r fname; do
-  # Only check text files with size > 0.
-  if [[ -s "${fname}" ]] && file --mime "${fname}" | grep -q " text/"; then
-    grep -HPn '[ \t]+$' "${fname}" >>"${tmpfile}" || true
+  # If pr_number or github_repository are empty, check all files.
+  if [[ -z "${pr_number}" ]] || [[ -z "${github_repository}" ]]; then
+    echo -e "Note: INPUT_PR_NUMBER or GITHUB_REPOSITORY not set. Checking all files.\n"
+    check_all_files=1
   fi
-done < "${CHANGED_FILES}"
 
-if [[ -s "${tmpfile}" ]]; then
-  echo "Found files containing lines with spaces or tabs at end-of-line."
-  echo "Please remove all lines containing only spaces and any extra"
-  echo "spaces or tabs after the last non-blank character in the files"
-  echo "and lines indicated below:"
-  echo
-  while read -r line; do
-    fname=$(echo "${line}" | cut -d: -f1)
-    linenum=$(echo "${line}" | cut -d: -f2)
-    echo "${fname}:${linenum}: Line contains trailing whitespaces."
-  done < "${tmpfile}"
-  exit 1
-fi
+  # Note: source_dir is ignored unless check_all_files is set.
+  if (( check_all_files )); then
+    fcmd=(find "${source_dir}" "-name" ".git" "-type" "d" "-prune" "-o" "-type" "f")
+    if [[ -n "${ignore_regex}" ]]; then
+      fcmd+=("!" "-regex" "${ignore_regex}")
+    fi
+    fcmd+=("-print")
+    "${fcmd[@]}" | sort >"${CHANGED_FILES}"
+  else
+    # Retrieve list of changes files.
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${pr_number}/files"
+    curl -s -X GET -G "${URL}" | jq -r '.[] | .filename' > "${CHANGED_FILES}"
+  fi
 
-echo "Test successful."
-exit 0
+  while read -r fname; do
+    if is_text_file "${fname}"; then
+      grep -HPn '[ \t]+$' "${fname}" >>"${tmpfile}" || true
+    fi
+  done < "${CHANGED_FILES}"
+
+  if [[ -s "${tmpfile}" ]]; then
+    ret=1
+    echo "Found files containing lines with spaces or tabs at end-of-line."
+    echo "Please replace all lines containing only spaces with empty lines"
+    echo "and remove any extra spaces or tabs after the last non-blank"
+    echo "character in the files and lines indicated below:"
+    echo "================================================================"
+    while read -r line; do
+      fname=$(echo "${line}" | cut -d: -f1)
+      linenum=$(echo "${line}" | cut -d: -f2)
+      echo "* ${fname}:${linenum}"
+    done < "${tmpfile}"
+    echo ""
+  fi
+
+  # Check for empty lines at EOF.
+  found=0
+  if (( check_empty_line_at_eof )); then
+    while read -r fname; do
+      is_text_file "${fname}" || continue
+
+      last_line="$(tail -n 1 "$fname")"
+      if [[ -z "$last_line" ]]; then
+        if (( ! found )); then
+          echo "Found files ending in blank lines. Please remove all blank"
+          echo "lines at the end of the files listed below:"
+          echo "=========================================================="
+          found=1
+          ret=1
+        fi
+        echo "* $fname"
+      fi
+    done < "${CHANGED_FILES}"
+
+    # Add a blank line to the output.
+    (( found )) && echo ""
+  fi
+
+  # Check for EOF_MISSING_NEWLINE.
+  found=0
+  if (( check_missing_newline_at_eof )); then
+    while read -r fname; do
+      is_text_file "${fname}" || continue
+
+      last_char="$(tail -c 1 "$fname")"
+      if [[ -n "$last_char" ]]; then
+        if (( ! found )); then
+          echo "Found files where the last line does not end in a newline."
+          echo "Please add a newline (not an empty line) to the files below."
+          echo ""
+          echo "TIP: If you're using VSCode, see this Stack Overflow question:"
+          echo "https://stackoverflow.com/questions/44704968/visual-studio-code-insert-newline-at-the-end-of-files"
+          echo "=============================================================="
+          found=1
+          ret=1
+        fi
+        echo "* $fname"
+      fi
+    done < "${CHANGED_FILES}"
+
+    # Add a blank line to the output.
+    (( found )) && echo ""
+  fi
+
+  return "${ret}"
+}
+
+main "$@"
